b7e53db60785a17a3a9bcf4ca2ad26f61475c2f3
Joe Alcorn <joe@joealcorn.co.uk>
2013-09-03 11:25:19 +0100

    Add a little more info to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='mock-neomodel',
     version='0.1',
+    url='https://github.com/buttscicles/mock-neomodel',
+    description='Basic mocking of neomodel objects',
+    license='MIT',
     packages=find_packages(),
     install_requires=['neomodel>=0.3', 'fudge>=1.0'],
+    classifiers=['License :: OSI Approved :: MIT License']
 )
